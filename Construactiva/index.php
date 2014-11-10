0e216b238b9455336efe92f7fb0b00e2f212dd46
Caleb Briancesco <caleb.briancesco@possible.com>
2014-11-09 21:22:41 -0600

    Homepage/sections functionality

--- Construactiva/index.php
+++ Construactiva/index.php
@@ -4,18 +4,207 @@
 <head>
     <meta charset="UTF-8">
     <title>ConstruActiva</title>
-    <link href="css/reset.css" rel="stylesheet" type="text/css" />
-    <link href="css/fonts.css" rel="stylesheet" type="text/css" />
-    <link href="css/defaults.css" rel="stylesheet" type="text/css" />
     
     <meta name="description" content="Construcción y Consultoría">
 	<meta name="viewport" content="width=device-width">
+    
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/fonts.css" rel="stylesheet" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Hind:300,700,400' rel='stylesheet' type='text/css' />
+    <link href="css/defaults.css" rel="stylesheet" type="text/css" />
+    
+    <!-- slider stylesheets -->
+	<link class="rs-file" href="api/royalslider/royalslider.css" rel="stylesheet">
+    <link class="rs-file" href="api/royalslider/skins/power_suplies/power_suplies_skin.css" rel="stylesheet">
+	
+    <!-- slider animation -->
+	<link class="rs-file" href="css/power_suplies.css" rel="stylesheet">    
 </head>
 
 <body>
     <header>
-        <h1 class="icon-construactiva">ConstruActiva Construcción y consultoría</h1>
+        <h1><span class="icon-construactiva"></span>
+            Constru<span>Activa</span> 
+            <span class="block">
+                Construccion &amp; consultoria
+            </span>
+        </h1>
+        
+        <nav class="mainNav">
+            <ul>
+                <li><a href="#" class="selected">Inicio</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Conozcanos</a></li>
+                <li><a href="#">Cotice</a></li>
+            </ul>
+        </nav>
     </header>
+    <div class="slider-holder">
+        <article class="slider">
+            <div id="construactivaSlide" class="royalSlider rsMinW">
+                <div class="rsContent slide1">
+                    <div class="bContainer">
+                        <img class="rsABlock piece1" data-fade-effect="true" data-move-effect="bottom" data-delay="1300" data-move-offset="100" data-easing="easeOutBack" data-speed="700" src="images/slider/slide1/piece1.png" alt="Piece" />
+
+                        <img class="rsABlock piece2" data-fade-effect="true" data-move-effect="bottom" data-delay="1000" data-move-offset="100" data-easing="easeOutBack" data-speed="1000"  src="images/slider/slide1/piece2.png" alt="Piece" />
+
+                        <img class="rsABlock piece3" data-fade-effect="true" data-move-effect="top" data-delay="1200" data-move-offset="100" data-easing="easeOutBack" data-speed="800" src="images/slider/slide1/piece3.png" alt="Piece" />
+
+                        <img class="rsABlock piece4" data-fade-effect="true" data-move-effect="top" data-delay="1000" data-move-offset="100" data-easing="easeOutBack" data-speed="1000" src="images/slider/slide1/piece4.png" alt="Piece" />
+
+                        <img class="rsABlock piece5" data-fade-effect="true" data-move-effect="top" data-delay="1100" data-move-offset="100" data-easing="easeOutBack" data-speed="900" src="images/slider/slide1/piece5.png" alt="Piece" />
+
+                        <img class="rsABlock bg1" data-fade-effect="true" data-move-effect="right" data-delay="0" data-move-offset="400" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide1/bg1.png" alt="Piece" />
+
+                        <h2 class="title1 rsABlock" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="100" data-easing="easeOutBack" data-speed="1000">Power Supplies for World Use</h2>
+
+                        <ul class="copy rsABlock list" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="100" data-easing="easeOutBack" data-speed="1000">
+                            <li>Comprehensive range of International power supplies</li>
+                            <li>Safety approved for global markets</li>
+                            <li>From stock or made to order</li>
+                        </ul>
+
+                        <a href="#" class="cta rsABlock" data-fade-effect="true" data-delay="2000" data-speed="1000" data-easing="easeOutBack" data-move-effect="none">More Info</a>
+                    </div>
+                </div>
+
+                <div class="rsContent slide2">
+                    <div class="bContainer">
+                        <img class="rsABlock piece1" data-fade-effect="true" data-move-effect="bottom" data-delay="1000" data-move-offset="45" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide2/pieces.png" alt="Piece" />
+
+                        <img class="rsABlock bg1" data-fade-effect="true" data-move-effect="left" data-delay="0" data-move-offset="300" data-easing="easeOutBack" data-speed="1000" src="images/slider/slide2/bg1.png" alt="Piece" />
+
+                        <h2 class="title1 rsABlock" data-fade-effect="true" data-move-effect="right" data-delay="500" data-move-offset="100" data-easing="easeOutBack" data-speed="1000">Desktop Power <span class="block">Supplies</span></h2>
+
+                        <ul class="copy rsABlock list" data-fade-effect="true" data-move-effect="right" data-delay="500" data-move-offset="100" data-easing="easeOutBack" data-speed="1000">
+                            <li>Power ratings from 1W to 280W</li>
+                            <li>Selection of output voltages from 5vdc to 48vdc</li>
+                            <li>Full international safety approvals</li>
+                            <li>All units meet latest energy efficiency standards</li>
+                        </ul>
+                        <a href="#" class="cta rsABlock" data-fade-effect="true" data-delay="2000" data-speed="1000" data-easing="easeOutBack" data-move-effect="none">More Info</a>
+                    </div>
+                </div>
+
+                <div class="rsContent slide3">
+                    <div class="bContainer">
+                        <img class="rsABlock piece1" data-fade-effect="true" data-move-effect="bottom" data-delay="1000" data-move-offset="45" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide3/piece1.png" alt="Piece" />
+
+                        <img class="rsABlock piece2" data-fade-effect="true" data-move-effect="top" data-delay="1000" data-move-offset="45" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide3/piece2.png" alt="Piece" />
+
+                        <img class="rsABlock bg1" data-fade-effect="true" data-move-effect="top" data-delay="0" data-move-offset="400" data-easing="easeOutBack" data-speed="1000" src="images/slider/slide3/bg1.png" alt="Piece" />
+
+                        <h2 class="title1 rsABlock" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="50" data-easing="easeOutBack" data-speed="1000">DC-DC
+                        <span class="block">Converters</span></h2>
+
+                        <ul class="copy rsABlock list" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="50" data-easing="easeOutBack" data-speed="1000">
+                            <li>Available from 0.25W to 40W</li>
+                            <li>Industry Standard Footprints</li>
+                            <li>Ultra-Fast Lead Time</li>
+                            <li>Safety Approved</li>
+                            <li>Aggressively priced
+
+                            <span class="block extraText">High power enclosed series 
+                            <span class="block">also available at up to 1000W</span></span></li>
+                        </ul>
+                        <a href="#" class="cta rsABlock" data-fade-effect="true" data-delay="2000" data-speed="1000" data-easing="easeOutBack" data-move-effect="none">More Info</a>
+                    </div>
+                </div>
+
+                <div class="rsContent slide4">
+                    <div class="bContainer">
+                        <img class="rsABlock piece1" data-fade-effect="true" data-move-effect="bottom" data-delay="1000" data-move-offset="100" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide4/piece1.png" alt="Piece" />
+
+                        <img class="rsABlock piece2" data-fade-effect="true" data-move-effect="top" data-delay="1000" data-move-offset="100" data-easing="easeOutBack" data-speed="1500" src="images/slider/slide4/piece2.png" alt="Piece" />
+
+                        <img class="rsABlock bg1" data-fade-effect="true" data-move-effect="left" data-delay="0" data-move-offset="500" data-easing="easeOutBack" data-speed="1000" src="images/slider/slide4/bg1.png" alt="Piece" />
+
+                        <h2 class="title1 rsABlock" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="80" data-easing="easeOutBack" data-speed="1000">LED Drivers</h2>
+
+                        <ul class="copy rsABlock list" data-fade-effect="true" data-move-effect="left" data-delay="500" data-move-offset="80" data-easing="easeOutBack" data-speed="1000">
+                            <li>For indoor or outdoor use</li>
+                            <li>Standard or Dimmable</li>
+                            <li>Wide range from 1W to 300W</li>
+                            <li>Constant Current or Constant Voltage outputs</li>
+                            <li>Up to 5 years warranty</li>
+                        </ul>
+                        <a href="#" class="cta rsABlock" data-fade-effect="true" data-delay="2000" data-speed="1000" data-easing="easeOutBack" data-move-effect="none">More Info</a>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+    
+    <section class="mainHolder">
+        <article class="contentHolder">
+            <ul class="sections">
+                <li>
+                    <h2>Vivienda a la medida</h2>
+                    <ul>
+                        <li>Nos ajustamos a su presupuesto</li>
+                        <li>Alcance en todo el país</li>
+                        <li>Atención personalizada</li>
+                        <li>Sistema llave en mano</li>
+                        <li>Remodelaciones</li>
+                    </ul>
+                    <a href="#">ver más sobre Vivienda a la medida</a>
+                </li>
+                
+                <li>
+                    <h2>Desarrollo Urbano</h2>
+                    <ul>
+                        <li>Nos ajustamos a su presupuesto</li>
+                        <li>Alcance en todo el país</li>
+                        <li>Atención personalizada</li>
+                        <li>Sistema llave en mano</li>
+                        <li>Remodelaciones</li>
+                    </ul>
+                    <a href="#">ver más sobre Desarrollo Urbano</a>
+                </li>
+                
+                <li>
+                    <h2>Tramites &amp; Consultoria</h2>
+                    <ul>
+                        <li>Nos ajustamos a su presupuesto</li>
+                        <li>Alcance en todo el país</li>
+                        <li>Atención personalizada</li>
+                        <li>Sistema llave en mano</li>
+                        <li>Remodelaciones</li>
+                    </ul>
+                    <a href="#">ver más sobre Trámites y Consultoría</a>
+                </li>
+            </ul>
+            
+            <div class="sectionsInfo">
+            </div>
+        </article>
+    </section>
+    
+    
+
+<!-- slider JS files -->
+    <script class="rs-file" src="api/royalslider/jquery-1.8.3.min.js"></script>
+    <script class="rs-file" src="api/royalslider/jquery.royalslider.min.js"></script>
+    <script class="rs-file" src="api/royalslider/jquery.easing-1.3.js"></script>
+    
+<script id="addJS">jQuery(document).ready(function($) {
+	jQuery.rsCSS3Easing.easeOutBack = 'cubic-bezier(0.175, 0.885, 0.320, 1.275)';
+	jQuery.rsCSS3Easing.easeOutBackCal = 'cubic-bezier(0.555, 0.090, 0.440, 0.965)';
+		$('#construactivaSlide').royalSlider({
+			arrowsNav: true,
+            arrowsNavAutoHide: false,
+            autoPlay: {
+                enabled: true,
+                pauseOnHover: true,
+                delay: 5000
+            },
+			loop: true,
+			numImagesToPreload: 4,
+			transitionType: 'slide',
+			keyboardNavEnabled: true
+		});
+	});
+</script>
     
 </body>
 
